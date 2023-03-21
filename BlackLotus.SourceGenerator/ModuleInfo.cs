742978760fc056f01757e3300b1696f927fe0293
Veerle Ulrich <v.ulrich@elabnext.com>
2023-03-21 20:19:01 +0100

    moduleinfo

---
+++ BlackLotus.SourceGenerator/ModuleInfo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace BlackLotus.SourceGenerator;
+
+public sealed class ModuleInfo : ISyntaxInfo, IEquatable<ModuleInfo>
+{
+    public ModuleInfo(string moduleName)
+    {
+        ModuleName = moduleName;
+    }
+
+    public string ModuleName { get; }
+
+
+    public bool Equals(ModuleInfo? other)
+    {
+        if (ReferenceEquals(null, other))
+        {
+            return false;
+        }
+
+        if (ReferenceEquals(this, other))
+        {
+            return true;
+        }
+
+        return ModuleName == other.ModuleName;
+    }
+
+    public override bool Equals(object? obj)
+        => ReferenceEquals(this, obj) ||
+           (obj is ModuleInfo other && Equals(other));
+    
+}
