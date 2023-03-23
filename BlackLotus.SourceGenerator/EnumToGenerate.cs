bf92c4e6eb98bd67f748b8ecef110cc74f9241fb
Veerle Ulrich <v.ulrich@elabnext.com>
2023-03-23 19:41:55 +0100

    enumgenerator example

---
+++ BlackLotus.SourceGenerator/EnumToGenerate.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace BlackLotus.SourceGenerator;
+
+public readonly struct EnumToGenerate
+{
+    public readonly string Name;
+    public readonly List<string> Values;
+
+    public EnumToGenerate(string name, List<string> values)
+    {
+        Name = name;
+        Values = values;
+    }
+}
